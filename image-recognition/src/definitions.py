b760df8c47edf75289ecd6f34a7412d7f6affd7d
v-zmiycharov <valentin.zmiycharov@gmail.com>
2016-10-18 17:15:36 +0300

    Setup list of urls

--- image-recognition/src/definitions.py
+++ image-recognition/src/definitions.py
@@ -1,4 +1,4 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(ROOT_DIR, os.pardir, "_DATA")
+DATA_DIR = os.path.join(ROOT_DIR, os.pardir, os.pardir, "_DATA")
