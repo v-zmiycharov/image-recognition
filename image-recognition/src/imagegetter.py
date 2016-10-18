a3bfe0dc112a8029b0fba502c28c1c106995ee99
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-10-18 22:48:14 +0300

    Change imagegetter to extract and delete tar file

--- image-recognition/src/imagegetter.py
+++ image-recognition/src/imagegetter.py
@@ -1,7 +1,8 @@
 import definitions
 import os
 import requests
-from time import gmtime, strftime
+from time import localtime, strftime
+import tarfile
 
 def generate_download_url(synset_id):
     return "http://image-net.org/download/synset?wnid={0}&username=vzmiycharov&accesskey=9fbd7b85ed46b46b2aed80e6335ac9ee5f5b9463&release=latest&src=stanford" \
@@ -14,21 +15,34 @@ def download_file(url, file_path):
             if chunk:
                 f.write(chunk)
 
-def download_archives():
-    with open(os.path.join(definitions.IMAGE_NET_DIR, "list.txt")) as list_file:
-        for line in list_file:
-            splitted = line.rstrip().split(" - ")
-            if len(splitted) == 2:
-                animal = splitted[0]
-                synset_id = splitted[1]
-                animal_dir = os.path.join(definitions.IMAGE_NET_DIR, animal)
-                if not os.path.exists(animal_dir):
-                    os.makedirs(animal_dir)
+def extract_tar_file(file_path):
+    if os.path.isfile(file_path):
+        with tarfile.open(file_path) as tar:
+            folder_path = os.path.dirname(file_path)
+            tar.extractall(path=folder_path)
 
-                url = generate_download_url(synset_id)
-                file_path = os.path.join(animal_dir, synset_id + ".tar")
-                print("{2}: Downloading {0} ({1}) ...".format(animal, synset_id, strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+with open(os.path.join(definitions.IMAGE_NET_DIR, "list.txt")) as list_file:
+    for line in list_file:
+        splitted = line.rstrip().split(" - ")
+        if len(splitted) == 2:
+            animal = splitted[0]
+            synset_id = splitted[1]
+            animal_dir = os.path.join(definitions.IMAGE_NET_DIR, animal)
+            print("{2}: {0} ({1}):".format(animal, synset_id, strftime("%Y-%m-%d %H:%M:%S", localtime())))
+            if not os.path.exists(animal_dir):
+                os.makedirs(animal_dir)
+
+            url = generate_download_url(synset_id)
+            file_path = os.path.join(animal_dir, synset_id + ".tar")
+
+            if len(os.listdir(animal_dir)) == 0:
+                print("{2}: Downloading {0} ({1}) ...".format(animal, synset_id, strftime("%Y-%m-%d %H:%M:%S", localtime())))
                 download_file(url, file_path)
-                print("{2}: Download {0} ({1}) finished".format(animal, synset_id, strftime("%Y-%m-%d %H:%M:%S", gmtime())))
 
-download_archives()
+            if os.path.isfile(file_path):
+                print("{2}: Extracting {0} ({1}) ...".format(animal, synset_id, strftime("%Y-%m-%d %H:%M:%S", localtime())))
+                extract_tar_file(file_path)
+                print("{2}: Deleting {0} ({1}) ...".format(animal, synset_id, strftime("%Y-%m-%d %H:%M:%S", localtime())))
+                os.remove(file_path)
+
+
