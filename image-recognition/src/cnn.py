7f3953be83819be981d9d1f09ffb137e88db0f3d
v-zmiycharov <valentin.zmiycharov@gmail.com>
2016-10-26 13:53:40 +0300

    Fix cnn to work

--- image-recognition/src/cnn.py
+++ image-recognition/src/cnn.py
@@ -6,34 +6,50 @@ from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import NeuralNet
 from PIL import Image
 import definitions
+from src.image_net import IMAGES
 
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
+IMAGE_SIZE = 28
+
+def img_to_numpy(img_path):
+    pic = Image.open(img_path)
+    pic = pic.resize((IMAGE_SIZE,IMAGE_SIZE), Image.ANTIALIAS)
+    np_array = np.array(pic)
+    return np_array.reshape((3,IMAGE_SIZE,IMAGE_SIZE))
+
 def load_dataset():
-    IMAGE_SIZE = 28
     X_train = []
     X_test = []
     y_train = []
     y_test = []
 
-    allowed_labels = ["alligator", "antelope"]
+    allowed_labels = IMAGES[:2]
 
     for label in allowed_labels:
-        train_dir = os.path.join(definitions.IMAGE_NET_DIR, label, definitions.IMAGES_DIR_NAME)
-        test_dir = os.path.join(definitions.IMAGE_NET_DIR, label, definitions.TEST_DIR_NAME)
+        train_dir = os.path.join(definitions.IMAGE_NET_DIR, label[1], definitions.IMAGES_DIR_NAME)
+        test_dir = os.path.join(definitions.IMAGE_NET_DIR, label[1], definitions.TEST_DIR_NAME)
         for filename in os.listdir(train_dir):
-            image = Image.open(os.path.join(train_dir, filename))
-            image = image.resize((IMAGE_SIZE, IMAGE_SIZE))
-            X_train.append(np.array(image))
-            y_train.append(label)
-        for filename in os.listdir(test_dir):
-            image = Image.open(os.path.join(test_dir, filename))
-            image = image.resize((IMAGE_SIZE, IMAGE_SIZE))
-            X_test.append(np.array(image))
-            y_test.append(label)
+            try:
+                image = img_to_numpy(os.path.join(train_dir, filename))
+            except ValueError as ve:
+                print(os.path.join(train_dir, filename))
+                continue
 
-    return X_train, np.array(y_train), X_test, np.array(y_test)
+            X_train.append(image)
+            y_train.append(label[0])
+        for filename in os.listdir(test_dir):
+            try:
+                image = img_to_numpy(os.path.join(test_dir, filename))
+            except ValueError as ve:
+                print(os.path.join(test_dir, filename))
+                continue
+
+            X_test.append(image)
+            y_test.append(label[0])
+
+    return np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test)
 
 X_train, y_train, X_test, y_test = load_dataset()
 
@@ -49,7 +65,7 @@ net1 = NeuralNet(
             ('output', layers.DenseLayer),
             ],
     # input layer
-    input_shape=(None, 1, 28, 28),
+    input_shape=(None, 3, IMAGE_SIZE, IMAGE_SIZE),
     # layer conv2d1
     conv2d1_num_filters=32,
     conv2d1_filter_size=(5, 5),
@@ -81,10 +97,13 @@ net1 = NeuralNet(
     verbose=1,
     )
 # Train the network
+print("Start train")
 nn = net1.fit(X_train, y_train)
 
+print("Predict")
 preds = net1.predict(X_test)
 
+print("Print results:")
 cm = confusion_matrix(y_test, preds)
 plt.matshow(cm)
 plt.title('Confusion matrix')
