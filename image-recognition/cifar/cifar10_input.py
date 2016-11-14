5a8f638caa8a67047893b0aaf4fdcfa3c3879345
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-11-14 23:06:41 +0200

    Add build train python files

--- image-recognition/cifar/cifar10_input.py
+++ image-recognition/cifar/cifar10_input.py
@@ -207,7 +207,7 @@ def inputs(eval_data, data_dir, batch_size):
   """
   if not eval_data:
     filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i)
-                 for i in xrange(1, 6)]
+                 for i in range(1, 6)]
     num_examples_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
   else:
     filenames = [os.path.join(data_dir, 'test_batch.bin')]
