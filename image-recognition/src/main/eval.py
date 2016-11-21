2697dcd84b7445f4f7899dd147ddaeecbd2d9ac0
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-11-21 09:45:06 +0200

    Load model & eval each test image

--- image-recognition/src/main/eval.py
+++ image-recognition/src/main/eval.py
@@ -4,6 +4,8 @@ import definitions
 import src.data.bin_generator as bin_generator
 import keras.models as k_models
 import numpy as np
+from keras.optimizers import SGD
+from data.imagenet_metadata import IMAGES
 
 MODEL = None
 
@@ -11,19 +13,33 @@ def load_model():
     global MODEL
     MODEL = k_models.load_model(os.path.join(definitions.MODEL_DATA_DIR, "keras_model.txt"))
 
+    decay = config.LEARN_RATE / config.EPOCHS
+    sgd = SGD(lr=config.LEARN_RATE, momentum=config.MOMENTUM, decay=decay, nesterov=False)
+    MODEL.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
 def predict(img_path):
     img_array = bin_generator.img_to_np(img_path, config.IMAGE_SIZE)
     return MODEL.predict(np.array([img_array]))
 
+def format_scores(scores):
+    for score_row in scores:
+        return "-".join(["%.2f" % score for score in score_row])
+
 if __name__ == '__main__':
     load_model()
 
-    img_paths = [
-        os.path.join(definitions.IMAGE_NET_DIR, "bear", definitions.TEST_DIR_NAME, "n02131653_577.JPEG"),
-        os.path.join(definitions.IMAGE_NET_DIR, "antelope", definitions.TEST_DIR_NAME, "n02419796_154.JPEG"),
-        os.path.join(definitions.IMAGE_NET_DIR, "antelope", definitions.TEST_DIR_NAME, "n02419796_1311.JPEG"),
-        os.path.join(definitions.IMAGE_NET_DIR, "bear", definitions.TEST_DIR_NAME, "n02131653_650.JPEG")
-    ]
+    for tuple in IMAGES[:config.NUM_CLASSES]:
+        animal = tuple[1]
+        print('----------', animal.upper(), '----------')
 
-    for path in img_paths:
-        print(predict(path), ": ", path)
+        dir_path = os.path.join(definitions.IMAGE_NET_DIR, animal, definitions.TEST_DIR_NAME)
+
+        for file_name in [file_name for file_name in os.listdir(dir_path)]:
+            path = os.path.join(dir_path, file_name)
+            try:
+                scores = predict(path)
+                print(format_scores(scores), file_name)
+            except IndexError as index_err:
+                print('---------- ERROR: ', file_name, '----------')
+
+        print('\n\n\n\n\n')
