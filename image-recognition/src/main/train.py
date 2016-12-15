8299c58138323c62a11ca61efaa3ce9e636d764b
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-12-15 10:24:13 +0200

    Update logging in model train to be for epoch only

--- image-recognition/src/main/train.py
+++ image-recognition/src/main/train.py
@@ -134,7 +134,7 @@ def train(folder):
     print(model.summary())
 
     # Fit the model
-    model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=config.EPOCHS, batch_size=config.BATCH_SIZE)
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=config.EPOCHS, batch_size=config.BATCH_SIZE, verbose=2)
     # Final evaluation of the model
     scores = model.evaluate(X_test, y_test, verbose=0)
     print("Accuracy: %.2f%%" % (scores[1] * 100))
