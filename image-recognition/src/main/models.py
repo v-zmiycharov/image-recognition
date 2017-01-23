1d80f1437bb5c692bd50bbe731c569cafc734103
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2017-01-23 22:44:15 +0200

    Move models filters parameters to variables

--- image-recognition/src/main/models.py
+++ image-recognition/src/main/models.py
@@ -9,41 +9,44 @@ from keras.constraints import maxnorm
 import src.config as config
 
 def create_vgg16_model(num_classes):
+    fs = 3
+    fc = 64
+    
     model = Sequential()
     model.add(ZeroPadding2D((1, 1), input_shape=(3, config.IMAGE_SIZE, config.IMAGE_SIZE)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc, fs, fs, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc, fs, fs, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc*2, fs, fs, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc*2, fs, fs, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc*4, fs, fs, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc*4, fs, fs, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc*4, fs, fs, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc*8, fs, fs, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc*8, fs, fs, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc*8, fs, fs, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc*8, fs, fs, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc*8, fs, fs, activation='relu'))
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(Convolution2D(fc*8, fs, fs, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
     model.add(Flatten())
@@ -55,12 +58,15 @@ def create_vgg16_model(num_classes):
     return model
 
 def create_simple_model(num_classes):
+    fs = 3
+    fc = 32
+
     model = Sequential()
-    model.add(Convolution2D(32, 3, 3, input_shape=(3, config.IMAGE_SIZE, config.IMAGE_SIZE), border_mode='same',
+    model.add(Convolution2D(fc, fs, fs, input_shape=(3, config.IMAGE_SIZE, config.IMAGE_SIZE), border_mode='same',
                             activation='relu',
                             W_constraint=maxnorm(3)))
     model.add(Dropout(0.2))
-    model.add(Convolution2D(32, 3, 3, activation='relu', border_mode='same', W_constraint=maxnorm(3)))
+    model.add(Convolution2D(fc, fs, fs, activation='relu', border_mode='same', W_constraint=maxnorm(3)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     model.add(Dense(512, activation='relu', W_constraint=maxnorm(3)))
@@ -69,18 +75,21 @@ def create_simple_model(num_classes):
     return model
 
 def create_complex_model(num_classes):
+    fs = 3
+    fc = 32
+
     model = Sequential()
-    model.add(Convolution2D(32, 3, 3, input_shape=(3, config.IMAGE_SIZE, config.IMAGE_SIZE), activation='relu', border_mode='same'))
+    model.add(Convolution2D(fc, fs, fs, input_shape=(3, config.IMAGE_SIZE, config.IMAGE_SIZE), activation='relu', border_mode='same'))
     model.add(Dropout(0.2))
-    model.add(Convolution2D(32, 3, 3, activation='relu', border_mode='same'))
+    model.add(Convolution2D(fc, fs, fs, activation='relu', border_mode='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Convolution2D(64, 3, 3, activation='relu', border_mode='same'))
+    model.add(Convolution2D(fc*2, fs, fs, activation='relu', border_mode='same'))
     model.add(Dropout(0.2))
-    model.add(Convolution2D(64, 3, 3, activation='relu', border_mode='same'))
+    model.add(Convolution2D(fc*2, fs, fs, activation='relu', border_mode='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Convolution2D(128, 3, 3, activation='relu', border_mode='same'))
+    model.add(Convolution2D(fc*4, fs, fs, activation='relu', border_mode='same'))
     model.add(Dropout(0.2))
-    model.add(Convolution2D(128, 3, 3, activation='relu', border_mode='same'))
+    model.add(Convolution2D(fc*4, fs, fs, activation='relu', border_mode='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     model.add(Dropout(0.2))
